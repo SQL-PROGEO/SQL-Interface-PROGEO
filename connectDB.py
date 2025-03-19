2d0996553d60ed44d3b59c134bf7cb96e43e68ce
Filipe Pinheiro <76874327+filipe-pinheiro@users.noreply.github.com>
2025-03-19 16:58:47 -0300

    Update connectDB.py

--- connectDB.py
+++ connectDB.py
@@ -11,45 +11,20 @@ oracle_client_path = os.path.join(base_path, "oracle", "instantclient_19_26")
 oracledb.init_oracle_client(lib_dir=oracle_client_path)
 
 def conectar_MA():
-    pw = "soltec"
 
-    connection = oracledb.connect(
-        user="ELETROMAPA",
-        password=pw,
-        dsn="10.1.1.217:1521/SDEMP")
-
-    cursor = connection.cursor()
     return connection, cursor
 
 def conectar_PA():
-    pw = "soltec"
-
-    connection = oracledb.connect(
-        user="ELETROMAPA",
-        password=pw,
-        dsn="10.130.3.17:1521/SDEMP")
 
     cursor = connection.cursor()
     return connection, cursor
 
 def conectar_PI():
-    pw = "soltec"
-
-    connection = oracledb.connect(
-        user="ELETROMAPA",
-        password=pw,
-        dsn="10.6.15.220:1521/SDEMP")
 
     cursor = connection.cursor()
     return connection, cursor
 
 def conectar_AL():
-    pw = "soltec"
-
-    connection = oracledb.connect(
-        user="ELETROMAPA",
-        password=pw,
-        dsn="10.6.11.11:1521/engpr")
 
     cursor = connection.cursor()
     return connection, cursor
